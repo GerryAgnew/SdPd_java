5050656dbfb0202fae5d0404ecf1808efee432af
GerryAgnew <GerryAgnew@users.noreply.github.com>
2015-05-19 14:09:32 +0100

    Create J02.java

---
+++ J02/J02.java
@@ -0,0 +1,21 @@
+// JdemoShell - Gerry Agnew
+
+// Empty Shell to avoid retyping class structure
+
+// import java.util.Scanner; // for I/O
+
+public class JdemoShell
+{
+	public static void main(String[] args )
+	{
+		// Scanner console = new Scanner(System.in); // for I/O
+
+		// code goes here ....
+
+		System.out.println("Empty Shell .....");
+
+		System.out.println(); // output a blank line
+
+	} // main
+
+} // JdemoShellter file contents here
