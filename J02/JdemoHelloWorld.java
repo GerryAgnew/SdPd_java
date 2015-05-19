18a08a4007e9a6eec2eb1a4b955dd07ac154d198
GerryAgnew <Gerry.Agnew@gmit.ie>
2015-05-19 13:18:35 +0100

    testing

---
+++ J02/JdemoHelloWorld.java
@@ -0,0 +1,17 @@
+
+// JdemoHelloWorld - Gerry Agnew
+// Demo Java Hello World Program
+
+public class JdemoHelloWorld
+{
+	public static void main(String[] args )
+	{
+		// Output Hello World message to monitor
+
+		System.out.println();
+		System.out.println("Gerry says Hello Java World !");
+		System.out.println();
+
+	} // main
+
+} // JdemoHelloWorld
